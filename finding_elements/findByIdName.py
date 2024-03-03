5b782d87f80ffa5daa32fb93b51ddf75d9de0be7
Ciprian <ciprian.tocu@heavensolutions.com>
2024-03-03 22:49:41 +0100

    clean up

---
+++ finding_elements/findByIdName.py
@@ -0,0 +1,23 @@
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+
+
+class FindByIdName:
+    def test(self):
+        baseUrl = 'https://www.letskodeit.com/practice'
+        driver = webdriver.Chrome()
+        driver.get(baseUrl)
+
+        driver.implicitly_wait(10)
+
+        elementById = driver.find_element(By.ID, 'displayed-text')
+        if elementById is not None:
+            print('Element found -> By Id')
+
+        elementByName = driver.find_element(By.NAME, 'show-hide')
+        if elementByName is not None:
+            print('Element found -> By Name')
+
+
+test_run = FindByIdName()
+test_run.test()
